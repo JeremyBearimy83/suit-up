f75f6ab9a1d52f83a71b6506c936be68be74a82a
Ujjwal Singhal <ujjwals@gmail.com>
2020-06-15 20:18:49 +0530

    divide the home page in to components and styled the home page

---
+++ src/pages/homepage/homepage.component.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+import DirectoryMenu from "../../components/directory-menu/directory-menu.components.jsx";
+import "./homepage.styles.scss";
+
+const Homepage = () => (
+  <div className="homepage">
+    <DirectoryMenu />
+  </div>
+);
+export default Homepage;
