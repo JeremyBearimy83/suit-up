b9d5dd44dc9fd478191a29181a4558d3dedbab74
Ujjwal Singhal <ujjwals@gmail.com>
2020-06-17 09:19:46 +0530

    make header component on all pages which used an svg logo

--- src/App.js
+++ src/App.js
@@ -3,10 +3,12 @@ import "./App.css";
 import Homepage from "./pages/homepage/homepage.component.jsx";
 import { Route, Switch } from "react-router-dom";
 import ShopPage from "./pages/shoppage/shoppage.component.jsx";
+import Header from "./components/header/header.component.jsx";
 
 function App() {
   return (
     <div>
+      <Header />
       <Switch>
         <Route exact path="/" component={Homepage}></Route>
         <Route exact path="/shop" component={ShopPage}></Route>
