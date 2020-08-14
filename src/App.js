20f4bda4d200fc43ca4d3f120d71f6d2004dc616
Ujjwal Singhal <ujjwals@gmail.com>
2020-08-14 11:31:13 +0530

    Redirect to homepage when user sign-in and integrate redux dev tools

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React, { Component } from "react";
 import "./App.css";
 import Homepage from "./pages/homepage/homepage.component.jsx";
-import { Route, Switch } from "react-router-dom";
+import { Route, Switch, Redirect } from "react-router-dom";
 import ShopPage from "./pages/shoppage/shoppage.component.jsx";
 import Header from "./components/header/header.component.jsx";
 import AuthStuff from "./pages/sign-in-and-sign-up/sign-in-and-sign-up.component.jsx";
@@ -38,14 +38,21 @@ class App extends Component {
         <Switch>
           <Route exact path="/" component={Homepage}></Route>
           <Route exact path="/shop" component={ShopPage}></Route>
-          <Route exact path="/sign-in" component={AuthStuff}></Route>
+          <Route
+            exact
+            path="/sign-in"
+            render={() =>
+              this.props.currentUser ? <Redirect to="/" /> : <AuthStuff />
+            }
+          ></Route>
         </Switch>
       </div>
     );
   }
 }
+const mapStateToProps = (state) => ({ currentUser: state.user.currentUser });
 const mapDispatchToProps = (dispatch) => ({
   setCurrentUser: (user) => dispatch(setCurrentUser(user)),
 });
 
-export default connect(null, mapDispatchToProps)(App);
+export default connect(mapStateToProps, mapDispatchToProps)(App);
