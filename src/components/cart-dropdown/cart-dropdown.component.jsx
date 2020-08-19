4fe8bc5c2940ff849c6e307d8f7f15838786bde0
Ujjwal Singhal <ujjwals@gmail.com>
2020-08-19 22:57:02 +0530

    Made and render cart item component in the cart dropdown

--- src/components/cart-dropdown/cart-dropdown.component.jsx
+++ src/components/cart-dropdown/cart-dropdown.component.jsx
@@ -1,13 +1,20 @@
 import React from "react";
 import CustomButton from "../custom-button/custom-button.component";
+import CartItem from "../cart-item/cart-item.component";
 import "./cart-dropdown.styles.scss";
+import { connect } from "react-redux";
 
-const CartDropdown = () => {
+const CartDropdown = ({ cartItems }) => {
   return (
     <div className="cart-dropdown">
-      <div className="cart-items"></div>
+      <div className="cart-items">
+        {cartItems.map((item) => (
+          <CartItem item={item} key={item.id} />
+        ))}
+      </div>
       <CustomButton>GO TO CHECKOUT</CustomButton>
     </div>
   );
 };
-export default CartDropdown;
+const mapStateToProps = (state) => ({ cartItems: state.cart.cartItems });
+export default connect(mapStateToProps)(CartDropdown);
