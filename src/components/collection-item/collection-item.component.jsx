eb0d0a2abdf4e3b1f9504e45c22b9ab089e0fb68
Ujjwal Singhal <ujjwals@gmail.com>
2020-08-19 18:14:49 +0530

    implement add router reducer and action, repeating cart items bug

--- src/components/collection-item/collection-item.component.jsx
+++ src/components/collection-item/collection-item.component.jsx
@@ -1,20 +1,31 @@
 import React from "react";
+
+import { connect } from "react-redux";
+
 import "./collection-item.styles.scss";
+
 import CustomButton from "../custom-button/custom-button.component";
 
+import { addItem } from "../../redux/cart/cart.actions";
+
 const CollectionItem = (props) => {
+  const { name, price, imageUrl } = props.item;
   return (
     <div className="collection-item">
-      <div
-        className="image"
-        style={{ backgroundImage: `url(${props.imageUrl})` }}
-      />
+      <div className="image" style={{ backgroundImage: `url(${imageUrl})` }} />
       <div className="collection-footer">
-        <span className="name">{props.name}</span>
-        <span className="price">{props.price}</span>
+        <span className="name">{name}</span>
+        <span className="price">{price}</span>
       </div>
-      <CustomButton inverted>add to cart</CustomButton>
+      <CustomButton inverted onClick={() => props.addItem(props.item)}>
+        add to cart
+      </CustomButton>
     </div>
   );
 };
-export default CollectionItem;
+
+const mapDispatchToProps = (dispatch) => ({
+  addItem: (item) => dispatch(addItem(item)),
+});
+
+export default connect(null, mapDispatchToProps)(CollectionItem);
