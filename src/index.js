15f39e79c99139ae7735807ac87a72093908a376
jeremybearimy83 <ujjwals@gmail.com>
2020-08-24 11:59:59 +0530

    Move directory component state (sections array) to redux

--- src/index.js
+++ src/index.js
@@ -1,15 +1,21 @@
 import React from "react";
+
 import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./App";
+
 import { BrowserRouter } from "react-router-dom";
 import { Provider } from "react-redux";
-import store from "./redux/store.js";
+import { store, persistor } from "./redux/store.js";
+
+import { PersistGate } from "redux-persist/integration/react";
 
 ReactDOM.render(
   <Provider store={store}>
     <BrowserRouter>
-      <App />
+      <PersistGate persistor={persistor}>
+        <App />
+      </PersistGate>
     </BrowserRouter>
   </Provider>,
 
